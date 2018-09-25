fd98a37a5c62738241093d267bad2f8cf918ae8d
Cranz <delacruzallanjay@gmail.com>
2018-09-25 19:05:02 +0800

    .net core post Update

---
+++ assets/js/site.js
@@ -0,0 +1,15 @@
+// import {MDCRipple} from '@material/ripple';
+
+// new MDCRipple(document.querySelector('.mdc-button'));
+
+// const buttons = document.querySelectorAll('.mdc-button');
+// for (const button of buttons) {
+//   mdc.ripple.MDCRipple.attachTo(button);
+// }
+
+
+
+// const buttons = document.querySelectorAll('.mdc-tab-ba');
+// for (const button of buttons) {
+//   mdc.ripple.MDCTabBar.attachTo(button);
+// }
